eeffe2f7c830ee4fd6eef212a0354eaf0c53c0d6
KwonSunwon <kso00011147@gmail.com>
2024-04-10 23:25:49 +0900

    feat: debug용 경계 추가

--- SpaceClicker/app/src/main/java/kr/ac/tukorea/ge/spgp/ksw/spaceclicker/GameView.java
+++ SpaceClicker/app/src/main/java/kr/ac/tukorea/ge/spgp/ksw/spaceclicker/GameView.java
@@ -2,6 +2,11 @@ package kr.ac.tukorea.ge.spgp.ksw.spaceclicker;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.view.View;
 
 public class GameView extends View {
@@ -9,6 +14,53 @@ public class GameView extends View {
 
     public GameView(Context context) {
         super(context);
-        this.activity = (Activity) context;
+        this.activity = (Activity)context;
+
+        borderPaint = new Paint();
+        borderPaint.setStyle(Paint.Style.STROKE);
+        borderPaint.setStrokeWidth(0.1f);
+        borderPaint.setColor(Color.RED);
+
+        setFullScreen(); // default behavior?
+    }
+
+    public void setFullScreen() {
+        int flags = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        setSystemUiVisibility(flags);
+    }
+
+    private static final float SCREEN_WIDTH = 9.0f;
+    private static final float SCREEN_HEIGHT = 16.0f;
+    private final PointF transformOffset = new PointF();
+    private float transformScale = 1;
+    private final RectF borderRect = new RectF(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    private final Paint borderPaint;
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        float view_ratio = (float)w / (float)h;
+        float game_ratio = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+        if (view_ratio > game_ratio) {
+            transformOffset.set((w - h * game_ratio) / 2, 0);
+            transformScale = h / SCREEN_HEIGHT;
+        } else {
+            transformOffset.set(0, (h - w / game_ratio) / 2);
+            transformScale = w / SCREEN_WIDTH;
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.save();
+        canvas.translate(transformOffset.x, transformOffset.y);
+        canvas.scale(transformScale, transformScale);
+        canvas.drawRect(borderRect, borderPaint);
+        canvas.restore();
     }
 }
