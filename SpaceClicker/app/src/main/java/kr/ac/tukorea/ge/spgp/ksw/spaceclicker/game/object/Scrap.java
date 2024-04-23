1bde347464b93d62f72f624baecd843a4a693f78
KwonSunwon <kso00011147@gmail.com>
2024-04-23 16:22:53 +0900

    feat: scrap이 시간에 따라 증가하는 기능 추가

--- SpaceClicker/app/src/main/java/kr/ac/tukorea/ge/spgp/ksw/spaceclicker/game/object/Scrap.java
+++ SpaceClicker/app/src/main/java/kr/ac/tukorea/ge/spgp/ksw/spaceclicker/game/object/Scrap.java
@@ -1,40 +1,52 @@
 package kr.ac.tukorea.ge.spgp.ksw.spaceclicker.game.object;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.RectF;
+import android.util.Log;
+import android.webkit.WebHistoryItem;
 
 import kr.ac.tukorea.ge.spgp.ksw.framework.interfaces.IGameObject;
 
 public class Scrap implements IGameObject {
+    private Point position;
+    private Paint paint;
+
     private long scrap;
     private long scrapPerTouch;
     private long scrapPerSecond;
 
-    public void Init() {
+    public Scrap() {
         // Load scrap from save file
-        scrap = 0;
+        scrap = 0; // if value is 100 that means player has 1 scrap, so 123 equals 1.23 scrap
         scrapPerTouch = 1;
-        scrapPerSecond = 0;
+        scrapPerSecond = 1;
+
+        position = new Point(2, 0);
+        paint = new Paint();
+        paint.setColor(Color.BLACK);
+        paint.setTextSize(1);
     }
 
     @Override
     public void update(float elapsedSeconds){
-
+        float scrapPerSecond = this.scrapPerSecond * elapsedSeconds;
+        Log.d("Scrap", "Scrap per second: " + scrapPerSecond);
+        scrap += scrapPerSecond * 100;
     }
 
     @Override
     public void draw(Canvas canvas){
-
+        canvas.drawText(GetScrap(), position.x, position.y, paint);
     }
 
     public void AddScrapFromClick() {
         scrap += scrapPerTouch;
     }
 
-    public void AddScrapFromSecond() {
-        scrap += scrapPerSecond;
-    }
-
     public String GetScrap() {
-        return Long.toUnsignedString(scrap);
+        return Long.toUnsignedString(scrap / 100);
     }
 }
