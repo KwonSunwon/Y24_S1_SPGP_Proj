36abb26ef711d119c2a1a93e45ad13fe882b9cc9
KwonSunwon <kso00011147@gmail.com>
2024-04-24 19:33:23 +0900

    feat: 스크랩 UI 아래쪽에 속도와 이동거리 나타내는 UI 추가

--- SpaceClicker/app/src/main/java/kr/ac/tukorea/ge/spgp/ksw/spaceclicker/game/object/SpaceShip.java
+++ SpaceClicker/app/src/main/java/kr/ac/tukorea/ge/spgp/ksw/spaceclicker/game/object/SpaceShip.java
@@ -9,19 +9,37 @@ import kr.ac.tukorea.ge.spgp.ksw.framework.view.Metrics;
 import kr.ac.tukorea.ge.spgp.ksw.spaceclicker.R;
 
 public class SpaceShip extends Sprite {
+    private long speed;
+    private long distance;
+
+    private ImageNumber speedDisplay;
+    private ImageNumber distanceDisplay;
+
     public SpaceShip() {
         super(R.mipmap.spaceship);
         setPosition(4.5f, 8.f, 3.f, 3.f );
+
+        // Load speed and distance from save file
+        speed = 100;
+        distance = 0;
+
+        speedDisplay = new ImageNumber(R.mipmap.font1, 8f, 1.5f, 0.3f);
+        distanceDisplay = new ImageNumber(R.mipmap.font1, 8f, 2.f, 0.3f);
     }
 
     @Override
     public void update(float elapsedSeconds) {
         super.update(elapsedSeconds);
+        distance += (long) (speed * elapsedSeconds);
+        speedDisplay.setNumber(speed);
+        distanceDisplay.setNumber(distance);
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+        speedDisplay.draw(canvas);
+        distanceDisplay.draw(canvas);
     }
 
     public boolean onTouch(MotionEvent event) {
