848079a6836618ab4abf41052ef4d92151bf7bc4
KwonSunwon <kso00011147@gmail.com>
2024-06-13 10:56:23 +0900

    refactor: bgObject를 scrollObj로 만들고 상속받아 제작하도록 변경

--- SpaceClicker/app/src/main/java/kr/ac/tukorea/ge/spgp/ksw/spaceclicker/game/object/RandomBgObject.java
+++ SpaceClicker/app/src/main/java/kr/ac/tukorea/ge/spgp/ksw/spaceclicker/game/object/RandomBgObject.java
@@ -13,33 +13,9 @@ import kr.ac.tukorea.ge.spgp.ksw.framework.view.Metrics;
 import kr.ac.tukorea.ge.spgp.ksw.spaceclicker.R;
 import kr.ac.tukorea.ge.spgp.ksw.spaceclicker.game.scene.MainScene;
 
-public class RandomBgObject extends Sprite implements IRecyclable, ITouchable {
+public class RandomBgObject extends ScrollObject implements ITouchable {
     public RandomBgObject() {
         super(R.mipmap.crate1);
-        // Set random x position
-        setPosition((float) Math.random() * 10, -5, 1, 1);
-        // Set random speed
-        dy = (float) Math.random() * 2 + 1;
-    }
-
-    @Override
-    public void update(float elapsedSeconds) {
-        super.update(elapsedSeconds);
-        if (y > 21) {
-            Scene.top().remove(MainScene.Layer.BACK_OBJECT, this);
-        }
-    }
-
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-    }
-
-    @Override
-    public void onRecycle() {
-        setPosition((float) Math.random() * 10, 0, 1, 1);
-        // Set random speed
-        dy = (float) Math.random() * 2 + 1;
     }
 
     @Override
---
+++ SpaceClicker/app/src/main/java/kr/ac/tukorea/ge/spgp/ksw/spaceclicker/game/object/ScrollObject.java
@@ -0,0 +1,28 @@
+package kr.ac.tukorea.ge.spgp.ksw.spaceclicker.game.object;
+
+import kr.ac.tukorea.ge.spgp.ksw.framework.interfaces.IRecyclable;
+import kr.ac.tukorea.ge.spgp.ksw.framework.objects.Sprite;
+import kr.ac.tukorea.ge.spgp.ksw.framework.scene.Scene;
+import kr.ac.tukorea.ge.spgp.ksw.spaceclicker.game.scene.MainScene;
+
+public class ScrollObject extends Sprite implements IRecyclable {
+    public ScrollObject(int mipmapId) {
+        super(mipmapId);
+        setPosition((float) Math.random() * 10, -5, 1, 1);
+        dy = (float) Math.random() * 2 + 1;
+    }
+
+    @Override
+    public void update(float elapsedSeconds) {
+        super.update(elapsedSeconds);
+        if (y > 21) {
+            Scene.top().remove(MainScene.Layer.BACK_OBJECT, this);
+        }
+    }
+
+    @Override
+    public void onRecycle() {
+        setPosition((float) Math.random() * 10, 0, 1, 1);
+        dy = (float) Math.random() * 2 + 1;
+    }
+}
